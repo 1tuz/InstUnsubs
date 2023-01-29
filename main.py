aec3ab0860042cf322ecc32f90308dffcdb5acb1
Antony <58532577+1tuz@users.noreply.github.com>
2023-01-29 17:10:36 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import requests
+
+# Replace YOUR_ACCESS_TOKEN with your actual Instagram access token
+access_token = 'YOUR_ACCESS_TOKEN'
+
+# Get the list of accounts you're subscribed to
+subscriptions_response = requests.get(f'https://api.instagram.com/v1/users/self/follows?access_token={access_token}')
+subscriptions = subscriptions_response.json()['data']
+subscription_ids = [subscription['id'] for subscription in subscriptions]
+
+# Get the list of accounts that subscribe to you
+followers_response = requests.get(f'https://api.instagram.com/v1/users/self/followed-by?access_token={access_token}')
+followers = followers_response.json()['data']
+follower_ids = [follower['id'] for follower in followers]
+
+# Get the list of accounts you're subscribed to but they're not subscribed back to you
+not_subscribed_back_ids = set(subscription_ids) - set(follower_ids)
+not_subscribed_back = [subscription for subscription in subscriptions if subscription['id'] in not_subscribed_back_ids]
+
+# Print the list of accounts
+print('Accounts you\'re subscribed to but they\'re not subscribed back to you:')
+for account in not_subscribed_back:
+    print(f'- {account["username"]}')
